14ae501de23f8ed205d9b8e7d50bb684316eafa4
Rajtilak Pandey <rajtilakpandey01@gmail.com>
2024-02-17 22:07:51 +0530

    bug fixes

--- client/src/screens/Loader.jsx
+++ client/src/screens/Loader.jsx
@@ -2,7 +2,9 @@ import {FadeLoader} from 'react-spinners'
 
 const Loader = () => {
   return (
-    <div className='bg-zinc-900 w-screen flex justify-center'><FadeLoader color='white'/></div>
+    <div>
+        <div className='bg-zinc-900 w-screen flex justify-center'><FadeLoader color='white'/></div>
+    </div>
   )
 }
 
